db6d8a5c0a7c37f526834e9a2c7b8a836f1cd5e3
Urs Stettler <u.stettler@linguala.com>
2022-05-22 14:11:38 +0200

    feat: 🎸 add news

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,11 +1,146 @@
 import React from "react";
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+//import required modules
+import { Parallax, Pagination, Navigation } from "swiper";
 
 export const News = () => {
+
+  //CSS
+  const Main = styled.div`
+    width: 100vw;
+    height: auto;
+    background: #87889c;
+    color: white;
+    font-family: "Roboto", sans-serif;
+  `;
+
+  const H3 = styled.h3`
+    padding-top: 2rem;
+    text-align: justify; 
+    color: white;
+    font-size: clamp(1.5rem, 2vw, 3rem);
+    font-weight: 500;
+    padding: 3rem 1.5rem 0 2rem;
+    text-align:left;
+    font-family: "Roboto", sans-serif;
+    `;
+
+
+
+  // Font
+  const H2 = styled.h2`
+    padding-top: 2rem;
+    color: white;
+    font-size: clamp(2rem, 2.5vw, 3.5rem);
+    font-weight: 600;
+    padding: 3rem 1.5rem 0 2rem;
+    text-align: center;
+    font-family: "Roboto", sans-serif;
+ 
+    
+  
+  @media only screen and (max-width : 600px) {
+    main{
+      height: 50vh;
+    } `;
+
   //Renderer
   return (
     <>
-      {/* Ab hier kommt das HTML rein */}
-      <h1>News</h1>
+      <Main>
+        {/* Ab hier kommt das HTML rein */}
+        <H2>News</H2>
+        <br />
+        <Swiper
+        style={{
+          "--swiper-navigation-color": "#fff",
+          "--swiper-pagination-color": "#fff",
+        }}
+        speed={600}
+        parallax={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Parallax, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <div
+          slot="container-start"
+          className="parallax-bg"
+          style={{
+            "background-image":
+              "url(https://swiperjs.com/demos/images/nature-1.jpg)",
+          }}
+          data-swiper-parallax="-23%"
+        ></div>
+        <SwiperSlide>
+          <div className="title" data-swiper-parallax="-300">
+           <H3> News 1</H3>
+          </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            VR Escape
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="title" data-swiper-parallax="-300">
+           <H3> News 2</H3>
+          </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            The Award!
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="title" data-swiper-parallax="-300">
+            <H3>News 3</H3>
+          </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            Best Game!
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            </p>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+        <br />
+      </Main>
     </>
   );
 };
