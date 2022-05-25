1922594c5eee5aef7c4da9cc81898fb91a3067c6
Cyrill Wyrsch <serialhd@gmail.com>
2022-05-25 20:05:32 +0200

    Newsletter & contact initial

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,6 @@
 // Import ähnmlich wie CDN
 import React from "react";
+import { Grid, TextField, Button, Card, CardContent, Typography } from '@mui/material';
 
 // Beginn vom Dokument am anfang kommt die Logik rein
 
@@ -8,7 +9,46 @@ export const Contact = () => {
   return (
     <>
       {/* Ab hier kommt das HTML rein */}
-      <h1>Contact</h1>
+      <div className="contact"> 
+      <Typography gutterBottom variant="h3" align="center">
+        Contact
+       </Typography>
+      <Grid>
+        <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5">
+              Get in touch with us
+          </Typography> 
+            <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+              Fill up the form and our team will get back to you ASAP.
+          </Typography> 
+            <form>
+              <Grid container spacing={1}>
+                <Grid xs={12} sm={6} item>
+                  <TextField placeholder="Enter first name" label="First Name" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid xs={12} sm={6} item>
+                  <TextField placeholder="Enter last name" label="Last Name" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField type="email" placeholder="Enter email" label="Email" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField type="number" placeholder="Enter phone number" label="Phone" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField label="Message" multiline rows={4} placeholder="Type your message here" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
+                </Grid>
+
+              </Grid>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </div>
     </>
   );
 };
--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -1,5 +1,8 @@
 // Import ähnmlich wie CDN
 import React from "react";
+import TextField from '@mui/material/TextField';
+import TextField from '@mui/material/Button';
+import newsletter_background from "../Newsletter/img/newsletter-background.png";
 
 // Beginn vom Dokument am anfang kommt die Logik rein
 
@@ -8,7 +11,26 @@ export const Newsletter = () => {
   return (
     <>
       {/* Ab hier kommt das HTML rein */}
-      <h1>Newsletter</h1>
+      
+
+      <div class="newsletter_container">
+
+        <div id="newsletter_box">
+          <h1>Für Newsletter anmelden</h1>
+          <TextField id="newsletter-subscription" label="Email" variant="standard" />
+        </div>
+
+        <img
+              src={newsletter_background}
+              alt="VR Escape Game"
+              style={{
+                width: "100%",
+                height: "auto",
+                backgroundSize: "cover",
+              }}
+            />
+      </div>
+
     </>
   );
 };
