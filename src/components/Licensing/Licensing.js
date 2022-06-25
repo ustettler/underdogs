bebac03dbc452feb61c0c2da1f322734cead5b10
Urs Stettler <u.stettler@linguala.com>
2022-06-25 16:06:13 +0200

    feat: 🎸 add licensing

--- src/components/Licensing/Licensing.js
+++ src/components/Licensing/Licensing.js
@@ -1,6 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import { useTable } from "react-table";
+//Mui
+import LinearScaleIcon from '@mui/icons-material/LinearScale';
+import PeopleIcon from '@mui/icons-material/People';
+import AvTimerIcon from '@mui/icons-material/AvTimer';
+import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import CelebrationIcon from '@mui/icons-material/Celebration';
 
 
 import useData from "./useData";
@@ -20,11 +27,28 @@ export const Licensing = () => {
     width: 100vw;
     height: auto;
     color: white;
+    display: flex;
+    justfy-centent: center;
+    align-items: center;
+    flex-direction: column;
     background-color: #525291;
     font-family: "Poppins", sans-serif;
     padding-left: 1rem;
     padding-right: 1rem;
   `;
+
+
+  const Requirement = styled.div`
+    
+    width: 50vw;
+    border: 3px solid white;
+    border-radius: 10px;
+    margin: 2rem 0 2rem 0;
+
+    @media only screen and (max-width : 600px) {
+        width: 90vw;
+      } `;
+
 
   // Font
 
@@ -46,34 +70,47 @@ export const Licensing = () => {
     padding: 3rem 1.5rem 0 0;
     text-align: center;
     font-family: "Poppins", sans-serif;
- 
-    
-  
-  @media only screen and (max-width : 600px) {
-    main{
-      height: 50vh;
-    } `;
+`;
+
+    const H3 = styled.h2`
+    padding-top: 1rem;
+    color: white;
+    font-size: clamp(1.2rem, 1.5vw, 2.5rem);
+    font-weight: 600;
+    padding: 1rem;
+    text-align: center;
+    font-family: "Poppins", sans-serif;
+    text-align: left;
+   
+    `;
 
   //Renderer
   return (
     <>
       <Main>     
         {/* Ab hier kommt das HTML rein */}
-        <H2>Pricing - B2B</H2>
+        <H2>Lizenzierung - B2B</H2>
         <P>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet.
-        </P>
-        <br />
+          Falls du eine VR-Center hast und unser Spiel gerne in dein Angebot aufnehmen möchtest, nehme gerne via 
+          Kontaktformular, mit uns kontakt auf.
+          </P>
+          <Requirement>
+          <H3> 
+          <strong>Folgendes sind die Voraussetzungen:</strong>
+          <br/>
+          <small>
+          <LinearScaleIcon/>&nbsp;&nbsp;10 x 10m Spielfläche<br/>
+ <PeopleIcon/>&nbsp;&nbsp; 2 - 5 Player<br/>
+          <AvTimerIcon/> &nbsp;&nbsp;Dauer: 30min<br/>
+          <VideogameAssetIcon/> &nbsp;&nbsp;Gamegenre: Escape Room<br/>
+          <CloudUploadIcon/>&nbsp;&nbsp;Gratis Deploy! (Octopus Deploy)<br />
+<CelebrationIcon/><b>&nbsp;&nbsp;Bereitschaft zufriede Kunden zu haben ;)</b><br />
+          </small>
+          </H3>
+        </Requirement>
+      
 
-        <div className="container">
+        {/* <div className="container">
           <table {...getTableProps()}>
             <thead>
               {headerGroups.map((headerGroup) => (
@@ -102,7 +139,7 @@ export const Licensing = () => {
               })}
             </tbody>
           </table>
-        </div>
+        </div> */}
         <br />
       </Main>
     </>
