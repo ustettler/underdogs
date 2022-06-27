3af926aed506b206f703f4ace0320611a3559117
Urs Stettler <u.stettler@linguala.com>
2022-06-27 21:19:48 +0200

    feat: 🎸 add video

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 //import SwissMap from "../Location/img/ch-karte.png";
 import { Parallax, Background } from "react-parallax";
-import IMG from "./img/hg.jpg";
+import IMG from "./img/web.gif";
 
 export const Location = () => {
   //CSS
@@ -51,7 +51,6 @@ export const Location = () => {
 
   // Font
   const H2 = styled.h2`
-    padding-top: 2rem;
     color: white;
     font-size: clamp(2rem, 2.5vw, 3.5rem);
     font-weight: 600;
@@ -63,11 +62,10 @@ export const Location = () => {
   `;
 
   const P = styled.p`
-    padding-top: 2rem;
     color: white;
     font-size: clamp(1rem, 1vw, 1.5rem);
     font-weight: 500;
-    padding: 3rem 1.5rem 0 2rem;
+    padding: 1rem 1.5rem 0 2rem;
     text-align: center;
     font-family: "Poppins", sans-serif;
     margin: auto;
@@ -77,11 +75,10 @@ export const Location = () => {
   `;
 
   const H3 = styled.h1`
-    padding-top: 1rem;
     color: white;
     font-size: clamp(1.5rem, 2vw, 3rem);
     font-weight: 500;
-    padding: 3rem 1.5rem 0 2rem;
+    padding: 1rem 1.5rem 0 2rem;
     text-align: center;
     font-family: "Poppins", sans-serif;
     text-shadow: 2px 0 0 black, -3px 0 0 black, 0 2px 0 black, 0 -2px 0 black, 1px 1px black, -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black;
@@ -131,87 +128,7 @@ export const Location = () => {
               ></iframe>
             </div>
             <br />
-            <div className="google-map-code">
-              <H3>
-                SAE Zürich <br />{" "}
-                <a
-                  href={"http://www.sae.edu/che/"}
-                  style={{
-                    textDecoration: "none",
-                    color: "#ffffff",
-                    fontSize: "1rem",
-                  }}
-                  target="_blank"
-                >
-                  Website
-                </a>
-              </H3>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2701.3940962684296!2d8.488158889559696!3d47.38474305460733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47900bcc0b75b2bf%3A0xb8c540464b5183c5!2sSAE%20Institute%20Z%C3%BCrich!5e0!3m2!1sde!2sfr!4v1656353049756!5m2!1sde!2sfr"
-                width="300"
-                height="225"
-                frameborder="0"
-                style={{ border: 0, margin: 20 }}
-                allowfullscreen=""
-                aria-hidden="false"
-                tabindex="0"
-              ></iframe>
-            </div>
-            <br />
-            <div className="google-map-code">
-              <H3>
-                SAE Zürich <br />{" "}
-                <a
-                  href={"http://www.sae.edu/che/"}
-                  style={{
-                    textDecoration: "none",
-                    color: "#ffffff",
-                    fontSize: "1rem",
-                  }}
-                  target="_blank"
-                >
-                  Website
-                </a>{" "}
-              </H3>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2701.3940962684296!2d8.488158889559696!3d47.38474305460733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47900bcc0b75b2bf%3A0xb8c540464b5183c5!2sSAE%20Institute%20Z%C3%BCrich!5e0!3m2!1sde!2sfr!4v1656353049756!5m2!1sde!2sfr"
-                width="300"
-                height="225"
-                frameborder="0"
-                style={{ border: 0, margin: 20 }}
-                allowfullscreen=""
-                aria-hidden="false"
-                tabindex="0"
-              ></iframe>
-            </div>
-        
-            <br />
-            <div className="google-map-code">
-              <H3>
-                SAE Zürich <br />{" "}
-                <a
-                  href={"http://www.sae.edu/che/"}
-                  style={{
-                    textDecoration: "none",
-                    color: "#ffffff",
-                    fontSize: "1rem",
-                  }}
-                  target="_blank"
-                >
-                  Website
-                </a>{" "}
-              </H3>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2701.3940962684296!2d8.488158889559696!3d47.38474305460733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47900bcc0b75b2bf%3A0xb8c540464b5183c5!2sSAE%20Institute%20Z%C3%BCrich!5e0!3m2!1sde!2sfr!4v1656353049756!5m2!1sde!2sfr"
-                width="300"
-                height="225"
-                frameborder="0"
-                style={{ border: 0, margin: 20 }}
-                allowfullscreen=""
-                aria-hidden="false"
-                tabindex="0"
-              ></iframe>
-            </div>
+           
           </Maps>
           <br />
         </Parallax>
