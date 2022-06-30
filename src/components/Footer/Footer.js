8f1995a39259cdaf34f70af79aadc4026e21643a
wafaa-kdouh <waffaa.maatouk@gmail.com>
2022-06-30 12:02:12 +0200

    Hero Video erstellen

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,8 +8,8 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+// import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
+// import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 
 export const Footer = () => {
 
@@ -37,54 +37,37 @@ export const Footer = () => {
 `;
 
   const Div = styled.div`
-    padding: 4rem 18rem;
     color: #fff;
+    position: relative;
+    left: 37%;
+    padding: 2rem 3rem;
+    margin: 1rem;
   @media (max-width: 768px) {
     padding: 1rem 3rem;
+    position: relative;
+    left: 0;
     transform: scale(0.8);
   }
   `;
 
-  const H4 = styled.h4`
-    font-size: 13px;
-    color: #ffffff;
-    text-transform: capitalize;
-    margin-bottom: 35px;
-    font-weight: 500;
-    position: relative;
-
-    ::before{
-      content: '';
-      position: absolute;
-      bottom: -10px;
-      background-color:#b7118a;
-      height: 2px;
-      box-sizing: border-box;
-      width: 50px;
-    }
-    @media (max-width: 768px) {
-      display:none
-    }
-  `;
-
-  const Ul = styled.ul`
-    list-style: none;
-    width: 14%;
-    padding: 1rem 15px;
-  `;
-  const Li = styled.li`
-    padding: 6px;
-  @media (max-width: 768px) {
-    padding: 1rem 3rem;
-    margin-left: -3rem;
-}
-`;
-
+  //   const Ul = styled.ul`
+  //     list-style: none;
+  //     width: 14%;
+  //     padding: 1rem 15px;
+  //   `;
+  //   const Li = styled.li`
+  //     padding: 6px;
+  //   @media (max-width: 768px) {
+  //     padding: 1rem 3rem;
+  //     margin-left: -3rem;
+  // }
+  // `;
 
   const Hr = styled.hr`
     width: 1889px;
     height: 0px;
     margin-left: 1rem;
+    margin-right: 1rem;
     background: rgba(255, 255, 255, 0.15);
   `;
 
@@ -93,7 +76,7 @@ export const Footer = () => {
     text-decoration: none;
     transition: all 0.3s ease-in;
     &:hover {
-      color :#b7118a;
+      color :#525291;
     }
 
   `;
@@ -115,16 +98,11 @@ export const Footer = () => {
   return (
     <>
       <Footer>
-        <Ul>
+        {/* <Ul>
           <H4>Adresse</H4>
           <Li><A href="#"><AddLocationAltIcon sx={{ fontSize: 40 }}></AddLocationAltIcon>Zürich</A></Li>
           <Li><A href="#"><LocalPhoneIcon sx={{ fontSize: 35 }}></LocalPhoneIcon>Telefon</A></Li>
-        </Ul>
-        <Ul>
-          <H4 >Adresse</H4>
-          <Li><A href="#"><AddLocationAltIcon sx={{ fontSize: 40 }}></AddLocationAltIcon>Zürich</A></Li>
-          <Li><A href="#"><LocalPhoneIcon sx={{ fontSize: 35 }}></LocalPhoneIcon>Telefon</A></Li>
-        </Ul>
+        </Ul> */}
         <Div>
           <A href="https://www.facebook.com/"> <FacebookIcon sx={{ fontSize: 50 }} ></FacebookIcon></A>
           <A href="https://www.facebook.com/"> <InstagramIcon sx={{ fontSize: 50 }}></InstagramIcon></A>
@@ -134,7 +112,7 @@ export const Footer = () => {
         <Logo href="">
           <img src={LogoImage} alt="Logo"
             style={{
-              width: "217px",
+              width: "200px",
             }}
           />
         </Logo>
