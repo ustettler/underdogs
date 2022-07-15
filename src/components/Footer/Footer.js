76e006eb4e0e2a1dee4f6dfeb20e757d8712eee9
Urs Stettler <u.stettler@linguala.com>
2022-07-15 18:00:30 +0200

    test: 💍 add impressum pop up

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,9 @@ import React from "react";
 // Styled Components Import
 import styled from "styled-components";
 import LogoImage from "../Footer/img/image (3).png"
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 // icon
 import FacebookIcon from '@mui/icons-material/Facebook';
@@ -95,6 +97,7 @@ export const Footer = () => {
     color:#fff;
 `;
 
+
   //Renderer
   return (
     <>
@@ -119,10 +122,32 @@ export const Footer = () => {
         </Logo>
         <Hr></Hr>
         <Wrapper>
-          <Text>&copy;the-underdogs 2022 | Impressum
-        
-</Text>
-        </Wrapper>
+          <Text>&copy;the-underdogs 2022 |  <Popup    trigger={open => (      <button className="button">Impressum {open ? '' : ''}</button>    )}    position="top left"    closeOnDocumentClick  >    <span> KONTAKTADRESSE
+Underdogs
+Buckhauserstrasse24
+8048 Zurich,
+Schweiz
+info@projectkennedy.ch
+VERTRETUNGS-<br/>
+BERECHTIGTE PERSONEN<br/>
+Hans Müller, <br/>Geschäftsführer
+HAFTUNGSAUSSCHLUSS
+Der Autor ubernimmt keinerlei Gewähr hinsichtlich der Inhaltlichen Richtigkeit, Genauigkeit, Aktualitat, Zuverlassigkeit und Vollständigkeit der Informationen
+Hattungsanspruche gegen den Autor wegen Schäden materieller oder immaterieller Art, welche aus dem Zugriff oder der Nutzuna bzw. Nichtnutzung der
+veröffentlichten Informationen, durch Missbrauch der Verbindung oder durch technische Störungen entstanden sind, werden ausgeschlossen. Alle Angebote sind
+unverbindlich. Der Autor behalt es sich ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu
+löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen<br/>
+HAFTUNG FÜR LINKS<br/>
+Verweise und Links auf Webseiten Dritter liegen ausserhalb unseres Verantwortungsbereichs. Es wird jegliche Verantwortung für solche Webseiten abgelehnt. Der
+Zugriff und die Nutzung solcher Webseiten erfolgen auf eigene Gefahr des Nutzers oder der Nutzerin.
+URHEBERRECHTE<br/>
+Die Urheber- und alle anderen Rechte an Inhalten. Bildern, Fotos oder anderen Dateien auf der Website gehören ausschliesslich der Firma Underdogs oder den
+speziell genannten Rechtsinhabern. Für die Reproduktion jeglicher Elemente ist die schriftliche Zustimmung der Urheberrechtsträger im Voraus einzuholer
+QUELLE<br/>
+Dieses Impressum wurde am 8.07.2022 mit dem Impressum Generator der Webdesign Agentur Webkönig erstellt. Die Agentur
+Webkönig übernimmt keine Haftung</span>  </Popup>
+            </Text>
+            </Wrapper>
       </Footer>
     </>
   );
